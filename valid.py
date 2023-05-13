1d708f428e5107fde9fb9544c294566dbb98fc27
VYRION <mh_882005@yahoo.com>
2023-05-14 00:07:10 +0300

    Update valid.py

--- valid.py
+++ valid.py
@@ -8,6 +8,7 @@ from super_gradients.training.metrics import DetectionMetrics_050
 from super_gradients.training.models.detection_models.pp_yolo_e import PPYoloEPostPredictionCallback
 # python train.py --project "pane" --data /content/data.yaml --location '/content/Dataset' --model-arch yolo_nas_s --batch-size 16 --max-epochs 25 --checkpoint-dir /content/checkpoints
 import locale
+from  google.colab.patches import cv2_imshow
 import os
 import yaml
 import sys
@@ -15,9 +16,12 @@ from pathlib import Path
 import supervision as sv
 import os
 import torch
-
+import cv2
+import os
 import numpy as np
-
+import random
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from onemetric.cv.object_detection import ConfusionMatrix
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -97,13 +101,60 @@ trainer.test(
         )
     )
 )
-#
-# ds = sv.Dataset.from_yolo(
-#     images_directory_path=dataset_params['test_images_dir'],
-#     annotations_directory_path=dataset_params['test_labels_dir'],
-#     data_yaml_path=args.data,
-#     force_masks=False
-# )
+
+ds = sv.Dataset.from_yolo(
+    images_directory_path=dataset_params['test_images_dir'],
+    annotations_directory_path=dataset_params['test_labels_dir'],
+    data_yaml_path=args.data,
+    force_masks=False
+)
+CONFIDENCE_TRESHOLD = 0.5
+
+predictions = {}
+
+for image_name, image in ds.images.items():
+    result = list(best_model.predict(image, conf=CONFIDENCE_TRESHOLD))[0]
+    detections = sv.Detections(
+        xyxy=result.prediction.bboxes_xyxy,
+        confidence=result.prediction.confidence,
+        class_id=result.prediction.labels.astype(int)
+    )
+    predictions[image_name] = detections
+random.seed(10)
+
+#####################################33
+MAX_IMAGE_COUNT = 5
+
+n = min(MAX_IMAGE_COUNT, len(ds.images))
+
+keys = list(ds.images.keys())
+keys = random.sample(keys, n)
+
+box_annotator = sv.BoxAnnotator()
+
+images = []
+titles = []
+
+for key in keys:
+    frame_with_annotations = box_annotator.annotate(
+        scene=ds.images[key].copy(),
+        detections=ds.annotations[key],
+        skip_label=True
+    )
+    images.append(frame_with_annotations)
+    titles.append('annotations')
+    frame_with_predictions = box_annotator.annotate(
+        scene=ds.images[key].copy(),
+        detections=predictions[key],
+        skip_label=True
+    )
+    images.append(frame_with_predictions)
+    titles.append('predictions')
+#print(type(images))
+#cv2.imwrite('image.jpg', images)
+#%matplotlib inline
+#sv.plot_images_grid(images=images, titles=titles, grid_size=(n, 2), size=(2 * 4, n * 4))
+###################3333
 # keys = list(ds.images.keys())
 #
 # annotation_batches, prediction_batches = [], []
@@ -115,6 +166,7 @@ trainer.test(
 #         annotation.class_id
 #     ))
 #     annotation_batches.append(annotation_batch)
+#     print(('key',key))
 #
 #     prediction=predictions[key]
 #     prediction_batch = np.column_stack((
